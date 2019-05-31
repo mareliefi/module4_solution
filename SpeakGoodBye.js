ff6074f8128e9c4bf428171e7e080429ea95dd8a
Marli Coetzee <mareliefies@gmail.com>
2019-05-31 14:01:56 +0200

    Changes made as per Mr Senior's request

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,6 +1,6 @@
 (function (window) {
   var byeSpeaker = {};
-  var speakWord = "Good Bye";
+  const speakWord = "Good Bye";
 
   byeSpeaker.speak = function(name) {
     console.log(speakWord + " " + name)
--- SpeakHello.js
+++ SpeakHello.js
@@ -1,6 +1,6 @@
 (function (window) {
   var helloSpeaker = {};
-  var speakWord = "Hello";
+  const speakWord = "Hello";
 
   helloSpeaker.speak = function(name) {
     console.log(speakWord + " " + name);
